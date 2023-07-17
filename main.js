62710a82771786e55cf0e4ec91a87ccf43eed773
dokikang <dokikang2015@gmail.com>
2023-07-17 17:18:45 +0200

    -

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ toggleBtn.addEventListener('click', () => {
 });
 
 /*--- Make home slowly fade to transparent as the window scrolls down ---*/
-const home = document.querySelector('#home');
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1-window.scrollY / homeHeight;
